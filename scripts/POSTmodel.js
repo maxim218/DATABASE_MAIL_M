00b886384f61fd286afe60960934020a5eb8ecca
maxim <maxim218maxim218@yandex.ru>
2018-04-08 16:27:08 +0300

    Adding pairs when add posts

--- scripts/POSTmodel.js
+++ scripts/POSTmodel.js
@@ -153,6 +153,8 @@ function createNewListOfPosts(response, threadIDorSLUG, bodyObj) {
 
                                             xxx = xxx + "(" + a1 + ",'" + a2 + "'," + a3 + ",'" + a4 + "','" + a5 + "'," + a6 + "," + a7 + ",'" + a8 + "'," + a9 + "," + a10 + "," + a11 + "," + a12 + "); ";
 
+                                            xxx = xxx + " SELECT * FROM create_new_pair(" + posts[i].author_id + ", " + FORUM_ID + ");";
+
                                             hugeString += xxx;
                                         }
 
