81b7a644e9cb7feb3036c73176b22d380c3a8128
maxim <maxim218maxim218@yandex.ru>
2018-05-09 04:27:50 +0300

    Add pair control

--- index.js
+++ index.js
@@ -341,6 +341,13 @@ function includeString(bigString, littleString) {
 
 let databaseCreated = false;
 
+function twoPartsService(part_2, value_2, part_4, value_4) {
+    if(part_2 !== value_2) {
+        return false;
+    }
+    return part_4 === value_4;
+}
+
 function getQuery(request, response) {
     if(request.url === "/api") {
         if(databaseCreated === false) {
@@ -375,13 +382,13 @@ function getQuery(request, response) {
 
     const argumentsArr = wordsArray(a1);
 
-    if(part_2 === "user" && part_4 === "profile") tryToGetInformationAboutUserInDatabase(request, response, part_3);
-    if(part_2 === "forum" && part_4 === "details") tryToGetForumInformation(request, response, part_3);
-    if(part_2 === "forum" && part_4 === "threads") tryToGetForumThreadsList(request, response, part_3, argumentsArr);
-    if(part_2 === "thread" && part_4 === "details") tryToGetFullInformationAboutOneThread(request, response, part_3);
-    if(part_2 === "thread" && part_4 === "posts") tryToGetListOfPostsFlatThreeParentThree(request, response, part_3, argumentsArr);
-    if(part_2 === "forum" && part_4 === "users") tryToGetAllStudentsThatHaveBranchPrPostInTheForum(request, response, part_3, argumentsArr);
-    if(part_2 === "post" && part_4 === "details") tryToGetInformationAboutOnePostSimple(request, response, part_3, argumentsArr);
+    if(twoPartsService(part_2,"user",part_4,"profile")) tryToGetInformationAboutUserInDatabase(request, response, part_3);
+    if(twoPartsService(part_2,"forum",part_4,"details")) tryToGetForumInformation(request, response, part_3);
+    if(twoPartsService(part_2,"forum",part_4,"threads")) tryToGetForumThreadsList(request, response, part_3, argumentsArr);
+    if(twoPartsService(part_2,"thread",part_4,"details")) tryToGetFullInformationAboutOneThread(request, response, part_3);
+    if(twoPartsService(part_2,"thread",part_4,"posts")) tryToGetListOfPostsFlatThreeParentThree(request, response, part_3, argumentsArr);
+    if(twoPartsService(part_2,"forum",part_4,"users")) tryToGetAllStudentsThatHaveBranchPrPostInTheForum(request, response, part_3, argumentsArr);
+    if(twoPartsService(part_2,"post",part_4,"details")) tryToGetInformationAboutOnePostSimple(request, response, part_3, argumentsArr);
 }
 
 function postQuery(request, response) {
@@ -411,13 +418,13 @@ function postQuery(request, response) {
         const part_3 = parts[3];
         const part_4 = parts[4];
 
-        if(part_2 === "user" && part_4 === "create") tryToAddUserToDatabase(request, response, mainObj, part_3);
-        if(part_2 === "user" && part_4 === "profile") tryToUpdateInformationAboutUser(request, response, mainObj, part_3);
-        if(part_2 === "forum" && part_4 === "create") tryToCreateThreadInForum(request, response, mainObj, part_3);
-        if(part_2 === "thread" && part_4 === "create") tryToAddBigListOfPosts(request, response, mainObj, part_3);
-        if(part_2 === "thread" && part_4 === "vote") tryToAddOrUpdateVoteOfUserToThread(request, response, mainObj, part_3);
-        if(part_2 === "thread" && part_4 === "details") tryToUpdateMessageOrTitleOfTheThread(request, response, mainObj, part_3);
-        if(part_2 === "post" && part_4 === "details") tryToUpdatePostMessageInComment(request, response, mainObj, part_3);
+        if(twoPartsService(part_2,"user",part_4,"create")) tryToAddUserToDatabase(request, response, mainObj, part_3);
+        if(twoPartsService(part_2,"user",part_4,"profile")) tryToUpdateInformationAboutUser(request, response, mainObj, part_3);
+        if(twoPartsService(part_2,"forum",part_4,"create")) tryToCreateThreadInForum(request, response, mainObj, part_3);
+        if(twoPartsService(part_2,"thread",part_4,"create")) tryToAddBigListOfPosts(request, response, mainObj, part_3);
+        if(twoPartsService(part_2,"thread",part_4,"vote")) tryToAddOrUpdateVoteOfUserToThread(request, response, mainObj, part_3);
+        if(twoPartsService(part_2,"thread",part_4,"details")) tryToUpdateMessageOrTitleOfTheThread(request, response, mainObj, part_3);
+        if(twoPartsService(part_2,"post",part_4,"details")) tryToUpdatePostMessageInComment(request, response, mainObj, part_3);
 
     });
 }
